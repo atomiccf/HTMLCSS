dac9ff56e992f41d7d21293c558ed3f17896589b
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2022-09-08 22:25:41 +0300

    add new task

---
+++ JavaScript_1/Anketa_if/anketa.js
@@ -0,0 +1,58 @@
+let surName = prompt("Введите вашу Фамилию", ' ');
+
+let name = prompt("Введите ваше Имя", ' ');
+
+let secondName = prompt("Введите ваше Отчество", ' ');
+
+let age = +prompt("Сколько вам лет?", ' ');
+
+let ageDays = age * 365;
+
+let ageFiveYears = age + 5;
+
+let retire;
+let currentGender;
+
+if (age > 120){
+    ageDays ="Похоже вы уже умерли!";
+    ageFiveYears ="Похоже вы уже умерли!"
+}else if (age<=0){
+    ageDays ="Вы еще не родились !";
+    ageFiveYears ="Вы еще не родились !"
+
+}
+let gender = confirm("Вы мужчина?")
+
+if (gender === true) {
+    currentGender = "мужской"
+
+} else {
+    currentGender = "женский"
+
+}
+
+
+if (gender === true || age >= 65 ) {
+    retire = "да";
+
+} else if (gender === false && age >= 58) {
+    retire = "да";
+
+} else {
+        retire = "нет";
+    }if (isNaN(age) === true) {
+    age = "введите корректные данные";
+    ageDays ="введите корректные данные";
+    ageFiveYears ="введите корректные данные"
+    retire = "введите корректные данные";
+}
+
+
+
+
+    alert("ваше ФИО: " + ' ' + surName + ' ' + name + ' ' + secondName +
+        '\nваш возраст в годах: ' + ' ' + age +
+        '\nваш возраст в днях: ' + ' ' + ageDays +
+        '\nчерез 5 лет вам будет: ' + ' ' + ageFiveYears +
+        '\nваш пол: ' + ' ' + currentGender +
+        '\nвы на пенсии: ' + ' ' + retire);
---
+++ add/HTML_CSS9/Flex_layout/switch.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $("#radio1").click(function() {
+        if($(this).is(":checked")) {
+            $(".item").css({transform : 'scale('+1.2+')'})
+
+        } else {
+            $(".item").css({transform : 'none'});
+
+        }
+
+
+    });
+    $("#radio2").click(function() {
+        if($(this).is(":checked")) {
+            $(".item").css({opacity :0});
+
+        } else {
+            $(".item").css({opacity :1});
+
+        }
+
+
+    });
+    $("#radio3").click(function() {
+        if($(this).is(":checked")) {
+            $(".item").css({transform : 'scale('+1.2+')'})
+
+        } else {
+            $(".item:nth-child(3)").css({transform : 'none'});
+
+        }
+
+
+    });
+
+});
+
+
+
+
+
+
+
+
