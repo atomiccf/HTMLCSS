20cda36eae72f823785e99e11eb56f1fbacaec89
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2022-10-06 19:07:04 +0300

    full adapt

--- add/HTML_CSS9/Flex_layout/switch.js
+++ add/HTML_CSS9/Flex_layout/switch.js
@@ -36,7 +36,12 @@ $(document).ready(function() {
 });
 
 
-
+$(document).ready (function (){
+    $('.header_burger').click(function (event){
+        $('.header_burger, .header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
 
 
 
